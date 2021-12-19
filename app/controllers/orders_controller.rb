905da0e5c1c90768b1d2c5a2b651e2547fd7f190
Solton33 <mid.night1202@gmail.com>
2021-12-19 18:21:49 +0900

    SoldOut表示のの実装

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -2,6 +2,23 @@ class OrdersController < ApplicationController
 
   def index
     @item = Item.find(params[:item_id])
+    @order = Order.new(order_params)
   end
 
+  def create
+   @item = Item.find(params[:item_id])
+   @order_address = OrderAddress.new(order_params)
+    if @order_address.valid?
+      @order_address.save
+      redirect_to root_path
+    else
+      render :index
+    end
+  end
+
+  private
+
+  def order_params
+    params.permit(:postal_code, :prefectures_id,:municipality, :address, :building, :phone_number, :item_id).merge(user_id: current_user.id)
+  end
 end
